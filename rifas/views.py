1f5cd8b50154a5834cddf739d3fb1376974dbd2b
Renan Sacca <renangs2222@gmail.com>
2020-10-22 17:16:20 -0300

    v2 commit

--- rifas/views.py
+++ rifas/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render,redirect
 from rifas.models import rifa
-
+from login.models import Profile
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 
 def rifas(request,valor):
     rifas = rifa.objects.filter(id=valor)
-    
+    profiles = Profile.objects.filter(user=request.user.id)
+    print(profiles)
     dados = {
         'rifa' : rifas,
+        'profiles': profiles,
     }
     
     return render(request,'rifas.html',dados)
